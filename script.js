64d01ea54f636ebe69f35040d7b341becc999836
Juniorawan06 <haseebawan.1560@gmail.com>
2024-06-11 05:17:39 +0500

    scene 7 done

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-
   gsap.registerPlugin(ScrollTrigger);
   let mm = gsap.matchMedia();
 
   mm.add("(min-width: 500px)", () => {
-
-
     const lenis = new Lenis({
       duration: 4,
       smoothWheel: true,
@@ -108,8 +105,8 @@ document.addEventListener("DOMContentLoaded", function () {
     gsap.to(".txt-three .line", {
       scrollTrigger: {
         trigger: ".txt-three",
-        start: "top 200px",
-        end: "+=500",
+        start: "top 35%",
+        end: "+=600",
         pin: true,
         toggleActions: "restart none reverse none",
       },
@@ -122,7 +119,7 @@ document.addEventListener("DOMContentLoaded", function () {
     gsap.to(".txt-four .line", {
       scrollTrigger: {
         trigger: ".txt-four",
-        start: "top 200px",
+        start: "top 35%",
         end: "+=400",
         pin: true,
         toggleActions: "restart none reverse none",
@@ -136,7 +133,7 @@ document.addEventListener("DOMContentLoaded", function () {
     gsap.to(".con-one .line", {
       scrollTrigger: {
         trigger: ".con-one",
-        start: "top 300px",
+        start: "top top",
         end: "+=500",
         pin: true,
         toggleActions: "restart none reverse none",
@@ -150,7 +147,7 @@ document.addEventListener("DOMContentLoaded", function () {
     gsap.to(".con-two .line", {
       scrollTrigger: {
         trigger: ".con-two",
-        start: "top 300px",
+        start: "top top",
         end: "+=500",
         pin: true,
         toggleActions: "restart none reverse none",
@@ -164,77 +161,7 @@ document.addEventListener("DOMContentLoaded", function () {
     gsap.to(".con-three .line", {
       scrollTrigger: {
         trigger: ".con-three",
-        start: "top 300px",
-        end: "+=500",
-        pin: true,
-        toggleActions: "restart none reverse none",
-      },
-      yPercent: 0,
-      duration: 1.7,
-      stagger: 0.09,
-      ease: "Expo.easeInOut",
-    });
-
-    gsap.to(".last-one .line", {
-      scrollTrigger: {
-        trigger: ".last-one",
-        start: "top center",
-        end: "+=500",
-        pin: true,
-        toggleActions: "restart none reverse none",
-      },
-      yPercent: 0,
-      duration: 1.7,
-      stagger: 0.09,
-      ease: "Expo.easeInOut",
-    });
-
-    gsap.to(".last-two .line", {
-      scrollTrigger: {
-        trigger: ".last-two",
-        start: "top 200px",
-        end: "+=500",
-        pin: true,
-        toggleActions: "restart none reverse none",
-      },
-      yPercent: 0,
-      duration: 1.7,
-      stagger: 0.09,
-      ease: "Expo.easeInOut",
-    });
-
-    gsap.to(".last-three .line", {
-      scrollTrigger: {
-        trigger: ".last-three",
-        start: "top 200px",
-        end: "+=500",
-        pin: true,
-        toggleActions: "restart none reverse none",
-      },
-      yPercent: 0,
-      duration: 1.7,
-      stagger: 0.09,
-      ease: "Expo.easeInOut",
-    });
-
-    gsap.to(".last-four .line", {
-      scrollTrigger: {
-        trigger: ".last-four",
-        start: "top 200px",
-        end: "+=500",
-        pin: true,
-        toggleActions: "restart none reverse none",
-      },
-      yPercent: 0,
-      duration: 1.7,
-      stagger: 0.09,
-      ease: "Expo.easeInOut",
-    });
-
-    gsap.to(".last-five .line", {
-      scrollTrigger: {
-        trigger: ".last-five",
-        start: "top 200px",
+        start: "top top",
         end: "+=500",
         pin: true,
         toggleActions: "restart none reverse none",
@@ -248,7 +175,7 @@ document.addEventListener("DOMContentLoaded", function () {
     gsap.to(".con-four .line", {
       scrollTrigger: {
         trigger: ".con-four",
-        start: "top 200px",
+        start: "top top",
         end: "+=450",
         pin: true,
         toggleActions: "restart none reverse none",
@@ -259,6 +186,75 @@ document.addEventListener("DOMContentLoaded", function () {
       ease: "Expo.easeInOut",
     });
 
+    gsap.to(".last-one .line", {
+      scrollTrigger: {
+        trigger: ".last-one",
+        start: "top top",
+        end: "+=500",
+        pin: true,
+        toggleActions: "restart none reverse none",
+      },
+      yPercent: 0,
+      duration: 1.7,
+      stagger: 0.09,
+      ease: "Expo.easeInOut",
+    });
+
+    gsap.to(".last-two .line", {
+      scrollTrigger: {
+        trigger: ".last-two",
+        start: "top top",
+        end: "+=500",
+        pin: true,
+        toggleActions: "restart none reverse none",
+      },
+      yPercent: 0,
+      duration: 1.7,
+      stagger: 0.09,
+      ease: "Expo.easeInOut",
+    });
+
+    gsap.to(".last-three .line", {
+      scrollTrigger: {
+        trigger: ".last-three",
+        start: "top top",
+        end: "+=500",
+        pin: true,
+        toggleActions: "restart none reverse none",
+      },
+      yPercent: 0,
+      duration: 1.7,
+      stagger: 0.09,
+      ease: "Expo.easeInOut",
+    });
+
+    gsap.to(".last-four .line", {
+      scrollTrigger: {
+        trigger: ".last-four",
+        start: "top top",
+        end: "+=500",
+        pin: true,
+        toggleActions: "restart none reverse none",
+      },
+      yPercent: 0,
+      duration: 1.7,
+      stagger: 0.09,
+      ease: "Expo.easeInOut",
+    });
+
+    gsap.to(".last-five .line", {
+      scrollTrigger: {
+        trigger: ".last-five",
+        start: "top top",
+        end: "+=500",
+        pin: true,
+        toggleActions: "restart none reverse none",
+      },
+      yPercent: 0,
+      duration: 1.7,
+      stagger: 0.09,
+      ease: "Expo.easeInOut",
+    });
 
     gsap.to(".base-img-wrapper", {
       scrollTrigger: {
@@ -269,8 +265,8 @@ document.addEventListener("DOMContentLoaded", function () {
         scrub: true,
       },
       delay: 1.5,
-      opacity: 0.3
-    })
+      opacity: 0.3,
+    });
 
     gsap.to(".txt-five .line", {
       scrollTrigger: {
@@ -278,7 +274,7 @@ document.addEventListener("DOMContentLoaded", function () {
         start: "top 100px",
         end: "+=600",
         pin: true,
-        toggleActions: "restart none reverse none"
+        toggleActions: "restart none reverse none",
       },
       yPercent: 0,
       duration: 1,
@@ -290,137 +286,158 @@ document.addEventListener("DOMContentLoaded", function () {
       scrollTrigger: {
         trigger: ".txt-six",
         start: "top 80%",
-        end: "+=380",
+        end: "+=100",
         pin: true,
-        toggleActions: "restart none reverse none"
+        toggleActions: "restart none reverse none",
       },
       yPercent: 0,
       duration: 1,
       stagger: 0.09,
       ease: "Expo.easeInOut",
     });
-
-    
   });
 
-    LottieScrollTrigger({
-      target: ".scene-one",
-      path: "https://lottie.host/4716a4fa-952f-4e6b-b635-a801cb0cd60f/WILaX0swxJ.json",
-      speed: "mediun",
-      scrub: 4,
-      // markers: true,
-    });
+  LottieScrollTrigger({
+    target: ".scene-one",
+    path: "https://lottie.host/4716a4fa-952f-4e6b-b635-a801cb0cd60f/WILaX0swxJ.json",
+    speed: "mediun",
+    scrub: 4,
+    // markers: true,
+  });
 
-    gsap.to(".connection", {
-      scrollTrigger: {
-        trigger: ".connection",
-        start: "top 80%",
-        end: "+=200",
+  gsap.to(".connection", {
+    scrollTrigger: {
+      trigger: ".connection",
+      start: "top 80%",
+      end: "+=200",
+      pin: true,
+      scrub: 3,
+      // markers: true
+    },
+    height: 310,
+    ease: "Expo.easeIn",
+  });
+
+  // ScrollTrigger.create({
+  //   trigger: ".connection",
+  //   start: "top 100px",
+  //   end: "+=1500",
+  //   pin: true,
+  //   // markers: true
+  // })
+
+  const races = document.querySelector(".sc-sv-img-wrapper");
+  console.log(races.offsetWidth);
+
+  function getScrollAmount() {
+    let racesWidth = races.scrollWidth;
+    return -(racesWidth - window.innerWidth);
+  }
+
+  const tween = gsap.to(races, {
+    x: getScrollAmount,
+    duration: 3,
+    ease: "none",
+  });
+
+  ScrollTrigger.create({
+    trigger: ".scene-seven-wrapper",
+    start: "top top",
+    end: () => `+=${getScrollAmount() * -1}`,
+    pin: true,
+    animation: tween,
+    scrub: 1,
+    invalidateOnRefresh: true,
+  });
+
+  LottieScrollTrigger({
+    target: ".scene-two",
+    path: "https://lottie.host/b60a2260-32b4-4b53-8bd0-b5040d12324b/eAH7C0xk3i.json",
+    speed: "slow",
+    scrub: 4,
+    // markers: true,
+  });
+
+  LottieScrollTrigger({
+    target: ".scene-three",
+    path: "https://lottie.host/976b0169-82ab-4118-9624-04075c170021/wS8cde9Gc5.json",
+    start: "top center",
+    speed: "slow",
+    scrub: 4,
+    // markers: true,
+  });
+
+  LottieScrollTrigger({
+    target: ".scene-four",
+    path: "https://lottie.host/21116ad4-df06-4b4c-ab21-fb567881962d/oSMtqw2UVp.json",
+    speed: "slow",
+    scrub: 4,
+    // markers: true,
+  });
+
+  LottieScrollTrigger({
+    target: ".scene-five",
+    path: "https://lottie.host/b5a27330-06d3-460d-ac7f-278b3e982263/PviNuyyT0k.json",
+    speed: "medium",
+    scrub: 4,
+  });
+
+  function LottieScrollTrigger(vars) {
+    let playhead = { frame: vars.startFrameOffset || 0 },
+      target = gsap.utils.toArray(vars.target)[0],
+      speeds = { slow: "+=1500", medium: "+=1000", fast: "+=500" },
+      st = {
+        trigger: target,
         pin: true,
-        scrub: 3,
-        // markers: true
+        start: "top top",
+        end: speeds[vars.speed] || "+=1000",
+        scrub: 1,
       },
-      height: 310,
-      ease: "Expo.easeIn",
-    });
-
-    // ScrollTrigger.create({
-    //   trigger: ".connection",
-    //   start: "top 100px",
-    //   end: "+=1500",
-    //   pin: true,
-    //   // markers: true
-    // })
-
-    LottieScrollTrigger({
-      target: ".scene-two",
-      path: "https://lottie.host/b60a2260-32b4-4b53-8bd0-b5040d12324b/eAH7C0xk3i.json",
-      speed: "slow",
-      scrub: 4,
-      // markers: true,
-    });
-
-    LottieScrollTrigger({
-      target: ".scene-three",
-      path: "https://lottie.host/976b0169-82ab-4118-9624-04075c170021/wS8cde9Gc5.json",
-      start: "top center",
-      speed: "slow",
-      scrub: 4,
-      // markers: true,
-    });
-
-    LottieScrollTrigger({
-      target: ".scene-four",
-      path: "https://lottie.host/21116ad4-df06-4b4c-ab21-fb567881962d/oSMtqw2UVp.json",
-      speed: "slow",
-      scrub: 4,
-      // markers: true,
-    });
-
-    LottieScrollTrigger({
-      target: ".scene-five",
-      path: "https://lottie.host/b5a27330-06d3-460d-ac7f-278b3e982263/PviNuyyT0k.json",
-      speed: "slow",
-      scrub: 4,
-    });
-
-    function LottieScrollTrigger(vars) {
-      let playhead = { frame: vars.startFrameOffset || 0 },
-        target = gsap.utils.toArray(vars.target)[0],
-        speeds = { slow: "+=1500", medium: "+=1000", fast: "+=500" },
-        st = {
-          trigger: target,
-          pin: true,
-          start: "top top",
-          end: speeds[vars.speed] || "+=1000",
-          scrub: 1,
+      ctx = gsap.context && gsap.context(),
+      animation = lottie.loadAnimation({
+        container: target,
+        renderer: vars.renderer || "svg",
+        loop: false,
+        autoplay: false,
+        path: vars.path,
+        rendererSettings: vars.rendererSettings || {
+          preserveAspectRatio: "xMidYMid slice",
         },
-        ctx = gsap.context && gsap.context(),
-        animation = lottie.loadAnimation({
-          container: target,
-          renderer: vars.renderer || "svg",
-          loop: false,
-          autoplay: false,
-          path: vars.path,
-          rendererSettings: vars.rendererSettings || {
-            preserveAspectRatio: "xMidYMid slice",
-          },
-        }),
-        frameAnimation;
-      for (let p in vars) {
-        // let users override the ScrollTrigger defaults
-        st[p] = vars[p];
-      }
-      frameAnimation = vars.timeline || gsap.timeline({ scrollTrigger: st });
-      if (vars.timeline && !vars.timeline.vars.scrollTrigger) {
-        // if the user passed in a timeline that didn't have a ScrollTrigger attached, create one.
-        st.animation = frameAnimation;
-        ScrollTrigger.create(st);
-      }
-      animation.addEventListener("DOMLoaded", function () {
-        let createTween = function () {
-          animation.goToAndStop(playhead.frame, true);
-          frameAnimation.to(
-            playhead,
-            {
-              frame: animation.totalFrames - 1 - (vars.endFrameOffset || 0),
-              ease: "none",
-              duration: frameAnimation.duration() || 1,
-              onUpdate: () => {
-                animation.goToAndStop(playhead.frame, true);
-              },
-            },
-            0
-          );
-          return () => animation.destroy && animation.destroy();
-        };
-        ctx && ctx.add ? ctx.add(createTween) : createTween();
-        // in case there are any other ScrollTriggers on the page and the loading of this Lottie asset caused layout changes
-        ScrollTrigger.sort();
-        ScrollTrigger.refresh();
-      });
-      animation.frameAnimation = frameAnimation;
-      return animation;
+      }),
+      frameAnimation;
+    for (let p in vars) {
+      // let users override the ScrollTrigger defaults
+      st[p] = vars[p];
     }
-
+    frameAnimation = vars.timeline || gsap.timeline({ scrollTrigger: st });
+    if (vars.timeline && !vars.timeline.vars.scrollTrigger) {
+      // if the user passed in a timeline that didn't have a ScrollTrigger attached, create one.
+      st.animation = frameAnimation;
+      ScrollTrigger.create(st);
+    }
+    animation.addEventListener("DOMLoaded", function () {
+      let createTween = function () {
+        animation.goToAndStop(playhead.frame, true);
+        frameAnimation.to(
+          playhead,
+          {
+            frame: animation.totalFrames - 1 - (vars.endFrameOffset || 0),
+            ease: "none",
+            duration: frameAnimation.duration() || 1,
+            onUpdate: () => {
+              animation.goToAndStop(playhead.frame, true);
+            },
+          },
+          0
+        );
+        return () => animation.destroy && animation.destroy();
+      };
+      ctx && ctx.add ? ctx.add(createTween) : createTween();
+      // in case there are any other ScrollTriggers on the page and the loading of this Lottie asset caused layout changes
+      ScrollTrigger.sort();
+      ScrollTrigger.refresh();
+    });
+    animation.frameAnimation = frameAnimation;
+    return animation;
+  }
 });
